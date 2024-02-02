cda5177d41a705d36787dfab637b36ed7b40ea95
Michael McAdow <michaelmcadow@gmail.com>
2024-02-02 06:12:17 -0500

    first push

---
+++ frontend/Laila Website/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Navbar from './components/Navbar'
+
+function App() {
+
+  return (
+    <div>
+      <Navbar/>
+      <h1>Hey there folks!</h1>
+    </div>
+    
+  )
+
+}
+
+export default App
