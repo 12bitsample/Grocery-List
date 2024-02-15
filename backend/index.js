d69b87a17c74c5a84f3fbe00e26ca6181dfcf848
Michael McAdow <michaelmcadow@gmail.com>
2024-02-15 06:27:56 -0500

    21524

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,13 @@ dotenv.config();
 import cors from 'cors';
 import express from 'express';
 import routes from './routes/authRoutes.js'
+import mongoose from 'mongoose';
 
+
+//db connection
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log('Database connected!'))
+.catch((err) => console.log('Database not connected!', err));
 
 const app = express();
 
