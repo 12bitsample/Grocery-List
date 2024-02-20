8329436802a3970c01583693785d4c09ed7d3014
Michael McAdow <michaelmcadow@gmail.com>
2024-02-20 11:20:54 -0500

    22024

--- frontend/Laila Website/src/pages/Register.jsx
+++ frontend/Laila Website/src/pages/Register.jsx
@@ -1,10 +1,13 @@
 import { useState } from 'react';
 import axios from 'axios';
-import { Toast } from 'react-hot-toast'; 
+import Toast from 'react-hot-toast'; 
+import { useNavigate } from 'react-router-dom'; 
 
 export default function Register() {
 
 
+  const navigate = useNavigate();
+  const toast = Toast();
   const [data, setData] = useState({
     name: '',
     email: '',
@@ -12,8 +15,39 @@ export default function Register() {
   });
 
   //handle input change
-  const handleChange = (e) => 
-    setData(prevState => ({prevState, [e.target.name]: e.target.value }));
+  // const handleChange = (e) => 
+  //   setData(prevState => ({prevState, [e.target.name]: e.target.value }));
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+
+    //update state depending on input name
+    switch (name) {
+      case 'name': 
+      setData(prevState => ({
+        ...prevState,
+        name: value
+      }));
+      break;
+      case 'email': 
+      setData(prevState => ({
+        ...prevState,
+        email: value
+      }));
+      break;
+      case 'password': 
+      setData(prevState => ({
+        ...prevState,
+        password: value
+      }));
+      break;
+      default: 
+      break;
+
+    }
+
+    
+  };
 
 
 
@@ -22,11 +56,22 @@ export default function Register() {
       const { name, email, password } = data;
 
       try{
-        const {data} = await axios.post('/register', {
+        const response = await axios.post('/register', {
           name, email, password
         })
+        if(response.data.error) {
+          toast.error(response.data.error);
+        } else {
+          setData({
+            name: '',
+            email: '',
+            password: '',
+          });
+          toast.success('Login successful!')
+          navigate('/login')
+        }
       } catch (error) {
-
+        console.log(error);
       }
   }
 
@@ -34,10 +79,10 @@ export default function Register() {
     <>
       <form onSubmit={registerUser}>
         <label>Register</label>
-        <input type="text" placeholder= "Enter name" value={data.name} onChange={handleChange}/>
-        <input type="email" placeholder="Enter email" value={data.email} onChange={handleChange}/>
-        <input type="password" placeholder="Enter password"value={data.password}onChange={handleChange}/>
-        <button type="submit">Submit</button>
+        <input type='text' placeholder= 'Enter name' value={data.name} onChange={handleChange} name='name' />
+        <input type='email' placeholder='Enter email' value={data.email} onChange={handleChange} name='email' />
+        <input type='password' placeholder='Enter password' value={data.password}onChange={handleChange} name='password' />
+        <button type='submit'>Submit</button>
 
       </form>
     </>
