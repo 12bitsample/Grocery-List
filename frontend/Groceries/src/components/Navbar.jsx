85290a0c61d53fe2518c1d5e5bf30ea59b8b7cf5
Michael McAdow <michaelmcadow@gmail.com>
2024-03-06 06:27:59 -0500

    3624

--- frontend/Groceries/src/components/Navbar.jsx
+++ frontend/Groceries/src/components/Navbar.jsx
@@ -14,13 +14,13 @@ const Navbar = () => {
         <nav className='navbar-nav navbar-expand-xxl bg-success' data-bs-theme='dark'>
             <div className='d-flex justify-content-between w-100 py-3'>
                 <div>
-                    <Link className='px-2 fw-bolder text-decoration-none'>Laila's Site</Link>
+                    <Link className='px-2 fw-bolder link-light text-decoration-none'>Grocery List Deluxe</Link>
                 </div>
                 
-                <div>
+                <div className='px-2'>
                     <Link className='px-1 text-decoration-none' to="/">Home</Link>
                     <Link className='px-1 text-decoration-none' to="/login">Login</Link>
-                    <Link className='px-1 text-decoration-none' to="/register">Register</Link>
+                    {/* <Link className='px-1 text-decoration-none' to="/register">Register</Link> */}
                 </div>
                 
             </div>
