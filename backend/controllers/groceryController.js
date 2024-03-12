3b4e0edb9bf44e2d8399a04a401a0aca7fdce09c
Michael McAdow <michaelmcadow@gmail.com>
2024-03-12 06:27:39 -0400

    31224

---
+++ backend/controllers/groceryController.js
@@ -0,0 +1,46 @@
+import GroceriesModel from "../models/groceriesModel.js";
+
+//get all groceries
+const getGroceries = async (req, res) => {
+    try {
+        const items = await GroceriesModel.find();
+        res.json(items);
+    } catch {
+        res.status(400).json({ error: error.message });
+    }
+}
+
+
+//add grocery item
+
+const addGroceryItem = async (req, res) => {
+    const {item, amount} = req.body;
+    
+    try {
+        const grocery = await GroceriesModel.create({item, amount});
+        res.status(200).json(grocery);
+    } catch (error) {
+        res.status(400).json({error: error.message });
+    }
+}
+//delete grocery item
+
+const deleteGroceryItem = async (req, res) => {
+    const { id } = req.params;
+
+    if (!mongoose.Types.ObjectId(id)) {
+        return res.status(404).json({error: 'Item can not be located.'});
+    }
+    
+    const groceryItem = await GroceriesModel.findOneAndDelete({_id: id});
+
+    if (!groceryItem) {
+        return res.status(400).json({error: 'Item can not be located.'});
+    }
+}
+
+export {
+    getGroceries,
+    addGroceryItem,
+    deleteGroceryItem,
+}
--- backend/routes/groceriesRoutes.js
+++ backend/routes/groceriesRoutes.js
@@ -1,28 +1,19 @@
 import express from "express";
-import GroceriesModel from "../models/groceriesModel.js";
+import mongoose from "mongoose";
+import { addGroceryItem, 
+        deleteGroceryItem, 
+        getGroceries, } from "../controllers/groceryController.js";
 
+//router
 const router = express.Router();
 
 //get all groceries items
-router.get('/', (req, res) => {
-    res.json({mssg: 'Get all groceries!'})
-});
+router.get('/', getGroceries);
 
 //post new grocery item
-router.post('/', async (req, res) => {
-    const {item, amount} = req.body;
-    
-    try {
-        const grocery = await Groceries.create({item, amount});
-        res.status(200).json(grocery);
-    } catch (error) {
-        res.status(400).json({error: error.message });
-    }
-});
+router.post('/', addGroceryItem);
 
 //delete grocery item
-router.delete('/:id', (req, res) => {
-    res.json({mssg: 'Delete grocery item!'})
-});
+router.delete('/:id', deleteGroceryItem);
 
 export {router as default};
