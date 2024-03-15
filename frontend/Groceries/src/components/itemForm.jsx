ae4f70f0bd60ec3bb14c6ca822a4a35c7b9b4251
Michael McAdow <michaelmcadow@gmail.com>
2024-03-15 09:55:41 -0400

    31524

---
+++ frontend/Groceries/src/components/itemForm.jsx
@@ -0,0 +1,12 @@
+
+
+const itemForm = () => {
+    const dispatch = use
+}
+
+    return (
+        <>
+        
+        
+        </>
+    )
--- frontend/Groceries/src/pages/List.jsx
+++ frontend/Groceries/src/pages/List.jsx
@@ -21,33 +21,25 @@ export default function List() {
             <div className="container-fluid">
                 <div className="row justify-content-center align-items-center h-100 w-75">
 
-                <div className="card border-success mb-3">
-                    <div className="card-header">Grocery List</div>
-                    <div className="card-body w-100 d-flex mx-auto justify-content-center">
-                        
-                    <ul className="list-group w-75">
-                        {groceries.map((grocery, index) => (
-                            <li key={grocery._id} className={`list-group-item d-flex justify-content-between align-items-center ${index % 2 === 0 ? 'list-group-item-success' : ''}`}>
+                    <div className="card border-success mb-3">
+                        <div className="card-header py-3"><h3>Grocery List</h3></div>
+                        <div className="card-body w-100 py-5 px-5 h-75 d-flex mx-auto justify-content-center">
+                            
+                            <ul className="list-group w-75">
+                                {groceries.map((grocery, index) => (
+                                    <li key={grocery._id} className={`list-group-item d-flex p-2 justify-content-between align-items-center ${index % 2 === 0 ? 'list-group-item-success' : ''}`}>
 
-                                <span className="grocery">{grocery.item}</span>
-                                <span className="badge bg-primary rounded-pill"><button type="button" class="btn btn-success">Delete</button></span>
-                            </li>
-                        ))}
-                    </ul>
+                                        <span className="grocery">{grocery.item}</span>
+                                        <span className="badge bg-primary rounded-pill"><button type="button" class="btn btn-success">Delete</button></span>
+                                    </li>
+                                ))}
+                            </ul>
 
 
+                        </div>
                     </div>
-                </div>
 
-                    {/* <ul className="list-group w-75">
-                        {groceries.map((grocery, index) => (
-                            <li key={grocery._id} className={`list-group-item d-flex justify-content-between align-items-center ${index % 2 === 0 ? 'list-group-item-success' : ''}`}>
 
-                                <span className="grocery">{grocery.item}</span>
-                                <span className="badge bg-primary rounded-pill"></span>
-                            </li>
-                        ))}
-                    </ul> */}
                 </div>
             </div>
 
