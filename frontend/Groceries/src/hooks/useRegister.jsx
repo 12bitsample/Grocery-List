c7699661dbb5d6bfccc72b74d55bde86bf73cb01
Michael McAdow <michaelmcadow@gmail.com>
2024-05-17 06:12:58 -0400

    51724

--- frontend/Groceries/src/hooks/useRegister.jsx
+++ frontend/Groceries/src/hooks/useRegister.jsx
@@ -1,8 +1,18 @@
+//not sure this is needed for thgis apps purposes
+
 import { useState } from "react";
 import { useAuthContext } from "./useAuthContext";
 
 export const useRegister = () => {
     const [ error, setError ] = useState(null);
     const [ isLoading, setIsLoading ] = useState(null);
-    
+
+    const register = async (email, password) => {
+        setIsLoading(true);
+        setError(null);
+
+        const response = await fetch('')
+    }
+
+
 }
