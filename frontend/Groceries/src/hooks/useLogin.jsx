b2d08b9ce8d1d927ef94d4ba7593949c008399be
Michael McAdow <michaelmcadow@gmail.com>
2024-07-19 06:08:55 -0400

    71924

--- frontend/Groceries/src/hooks/useLogin.jsx
+++ frontend/Groceries/src/hooks/useLogin.jsx
@@ -3,8 +3,36 @@ import { useAuthContext } from "./useAuthContext";
 
 export const useSignup = () => {
     const [error, setError] = useState(null);
-    const [loading, isLoading] = useState(null);
+    const [loading, setIsLoading] = useState(null);
     const { dispatch } = useAuthContext();
     
+    const login = async (email, password) => {
+        setIsLoading(true);
+        setError(null);
+
+        const response = await fetch('api/user/login', {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({email, password}),
+        })
+
+        const json = await response.json();
+
+        if (!response.ok) {
+            setIsLoading(false);
+            setError(json.error);
+        }
+
+        if (response.ok) {
+            //save user to local storage
+            localStorage.setItem('user', JSON.stringify(json));
+
+            //update auth context
+            dispatch({type: 'LOGIN', payload: json});
+
+            setIsLoading(false);
+        }
+
+    }
 
 }
