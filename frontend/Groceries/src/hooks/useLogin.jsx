261f3ea45c99c268d37ffa48a488ba16c0c40d96
Michael McAdow <michaelmcadow@gmail.com>
2024-07-22 05:43:19 -0400

    72224

--- frontend/Groceries/src/hooks/useLogin.jsx
+++ frontend/Groceries/src/hooks/useLogin.jsx
@@ -1,9 +1,10 @@
 import { useState } from "react";
 import { useAuthContext } from "./useAuthContext";
 
-export const useSignup = () => {
+
+export const useLogin = () => {
     const [error, setError] = useState(null);
-    const [loading, setIsLoading] = useState(null);
+    const [isLoading, setIsLoading] = useState(null);
     const { dispatch } = useAuthContext();
     
     const login = async (email, password) => {
@@ -32,7 +33,7 @@ export const useSignup = () => {
 
             setIsLoading(false);
         }
-
     }
 
+    return { login, isLoading, error }
 }
--- frontend/Groceries/src/pages/Login.jsx
+++ frontend/Groceries/src/pages/Login.jsx
@@ -1,3 +1,39 @@
+//need to redo Login component!!!
+
+// import { useState } from 'react';
+// import axios from 'axios';
+
+// export default function Login() {
+
+//   const [data, setData] = useState({
+//     email: '',
+//     password: '',
+//   })
+
+//   //handle input change
+//   const handleChange = (e) => setData(prevState => ({prevState, [e.target.name]: e.target.value }));
+
+//   //login user
+//   const loginUser = (e) => {
+//     e.preventDefault();
+//     // axios.get('/');
+//     alert('You have attempted to login.')
+//   }
+
+//   return (
+//     <>
+//       <div className='container-fluid' data-bs-theme='light'>
+//         <form className='form-vertical-center p-4 background-gradient rounded' onSubmit={loginUser}>
+//           <label className='pb-1 fw-bold text-light'>Login</label>
+//           <input className='my-1' type="email" placeholder="Enter email" value={data.email} onChange={(e) => setData({...data, email: e.target.value})}/>
+//           <input className='my-1' type="password"placeholder="Enter password"value={data.password}onChange={(e) => setData({...data, password: e.target.value})}/>
+//           <button className='my-1' type="submit">Login</button>
+//         </form>
+//       </div>
+//    </>
+//   )
+// }
+
 import { useState } from 'react';
 import axios from 'axios';
 
