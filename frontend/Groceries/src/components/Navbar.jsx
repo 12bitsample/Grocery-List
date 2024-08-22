76403afd563e6723f28d02481d4f8e1dd8de3788
Michael McAdow <michaelmcadow@gmail.com>
2024-08-22 06:12:10 -0400

    82224

--- frontend/Groceries/src/components/Navbar.jsx
+++ frontend/Groceries/src/components/Navbar.jsx
@@ -1,10 +1,13 @@
 import { Link } from 'react-router-dom';
 import { useLogout } from '../hooks/useLogout';
+import { useAuthContext } from '../hooks/useAuthContext';
 
 
 const Navbar = () => {
 
+    const { user } = useAuthContext();
     const { logout } = useLogout();
+
 
     const handleClick = () => {
         logout();
