d19f0f693f2925ce66c9083a0cec8d3ca07f8dfb
Michael McAdow <michaelmcadow@gmail.com>
2024-09-04 06:05:48 -0400

    9424

--- frontend/Groceries/src/components/Navbar.jsx
+++ frontend/Groceries/src/components/Navbar.jsx
@@ -15,13 +15,13 @@ const Navbar = () => {
 
     return (
 
-        <nav className='navbar-nav navbar-expand-xxl background-gradient' data-bs-theme='dark'>
-            <div className='d-flex justify-content-between w-100 py-3'>
+        <nav className="navbar-nav navbar-expand-xxl background-gradient" data-bs-theme="dark">
+            <div className="d-flex justify-content-between w-100 py-3">
                 <div>
-                    <Link className='px-2 fw-bolder link-light text-decoration-none' to='/'>Grocery List Deluxe</Link>
+                    <Link className="px-2 fw-bolder link-light text-decoration-none" to='/'>Grocery List Deluxe</Link>
                 </div>
-                <div className='px-2'>
-                    <div className="column">
+                <div className="px-1 align-items-end">
+                    <div className="row d-flex">
                         <span>
                             <div className="column">
                                 <Link className='px-1 fw-bold link-light text-decoration-none' to="/">Home</Link>
@@ -31,10 +31,10 @@ const Navbar = () => {
                             </div>
                         </span>
 
-                        <div className="column d-flex">
+                        <div className="d-flex flex-column align-items-center">
                                     {user ? (<p className="col text-light text-nowrap align-middle">Welcome, {user.email}</p>) : (<Link className='px-1 fw-lighter link-light text-decoration-none' to="/login">Please log in!</Link>)
                                     }
-                                    {user ? (<div className="col py-1 d-flex justify-content-end">
+                                    {user ? (<div className="col py-1 d-flex justify-content-end align-middle">
                                         <button onClick={handleClick} type="button" className="btn fw-bold p-1 btn-outline-secondary">Logout?</button>
                                         </div>) : (<p></p>)
                                     }  
