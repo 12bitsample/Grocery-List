2209f870fb1168d55311f3d6d1e41f2d90a47af6
Michael McAdow <michaelmcadow@gmail.com>
2024-09-06 05:40:14 -0400

    9624

--- frontend/Groceries/src/components/Navbar.jsx
+++ frontend/Groceries/src/components/Navbar.jsx
@@ -2,12 +2,10 @@ import { Link } from 'react-router-dom';
 import { useLogout } from '../hooks/useLogout';
 import { useAuthContext } from '../hooks/useAuthContext';
 
-
 const Navbar = () => {
 
     const { user } = useAuthContext();
     const { logout } = useLogout();
-
 
     const handleClick = () => {
         logout();
@@ -22,25 +20,24 @@ const Navbar = () => {
                 </div>
                 <div className="d-flex px-1">
                     <div className="row d-flex">
-                        <span>
-                            <div className="flex-column d-flex align-items-end">
-                                <Link className="px-1 fw-bold link-light text-decoration-none" to="/">Home</Link>
-                                <Link className="px-1 fw-bold link-light text-decoration-none" to="/register">Register</Link>
-                                <Link className="px-1 fw-bold link-light text-decoration-none" to="/login">Login</Link>
-                                <Link className="px-1 fw-bold link-light text-decoration-none" to="/list">List</Link>
-                            </div>
-                        </span>
-
                         <div className="d-flex flex-column align-items-end">
+
+                            <span>
+                                <div className="column d-flex align-items-end">
+                                    <Link className="px-1 fw-bold link-light text-decoration-none" to="/">Home</Link>
+                                    <Link className="px-1 fw-bold link-light text-decoration-none" to="/register">Register</Link>
+                                    <Link className="px-1 fw-bold link-light text-decoration-none" to="/login">Login</Link>
+                                    <Link className="px-1 fw-bold link-light text-decoration-none" to="/list">List</Link>
+                                </div>
+                            </span>
+
                                     {user ? (<p className="col text-light text-nowrap align-middle">Welcome, {user.email}</p>) : (<Link className='px-1 fw-lighter link-light text-decoration-none' to="/login">Please log in!</Link>)
                                     }
                                     {user ? (<div className="col py-1 d-flex justify-content-end align-middle">
                                         <button onClick={handleClick} type="button" className="btn fw-bold p-1 btn-outline-secondary">Logout?</button>
                                         </div>) : (<p></p>)
                                     }  
-                                </div>
-                        
-
+                                 </div>
                     </div>
                 </div>
             </div>
