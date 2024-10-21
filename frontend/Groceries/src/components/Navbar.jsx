6bd891beaebd95f804165e0991fb63d239d0dc03
Michael McAdow <michaelmcadow@gmail.com>
2024-10-21 05:55:54 -0400

    102124

--- frontend/Groceries/src/components/Navbar.jsx
+++ frontend/Groceries/src/components/Navbar.jsx
@@ -1,17 +1,17 @@
 import { Link } from "react-router-dom";
 import { useLogout } from "../hooks/useLogout";
 import { useAuthContext } from "../hooks/useAuthContext";
-import { useNavigate } from "react-router-dom";
+// import { useNavigate } from "react-router-dom";
 
 const Navbar = () => {
 
     const { logout } = useLogout();
     const { user } = useAuthContext();
-    const { navigate } = useNavigate();
+    // const { navigate } = useNavigate();
 
     const handleClick = () => {
         logout();
-        navigate("/");
+        // navigate("/");
     }
 
     return (
--- frontend/Groceries/src/hooks/useLogout.jsx
+++ frontend/Groceries/src/hooks/useLogout.jsx
@@ -1,14 +1,19 @@
 import { useAuthContext } from "./useAuthContext";
+import { useNavigate } from "react-router-dom";
 
 export const useLogout = () => {
     const { dispatch } = useAuthContext();
+    const navigate = useNavigate();
 
     const logout = () => {
         //remove user from storage
         localStorage.removeItem("user");
 
         //dispatch logout action
-        dispatch({type: "LOGOUT"})
+        dispatch({type: "LOGOUT"});
+
+        //navigate to home
+        navigate("/");
     }
 
     return {logout};
