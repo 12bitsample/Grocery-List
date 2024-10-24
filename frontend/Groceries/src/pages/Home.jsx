225adfbd1d36602de436720034514c85681b4863
Michael McAdow <michaelmcadow@gmail.com>
2024-10-24 05:46:24 -0400

    102424

--- frontend/Groceries/src/pages/Home.jsx
+++ frontend/Groceries/src/pages/Home.jsx
@@ -15,13 +15,22 @@ export default function Home() {
         {
           user ? (
             <div className="card border-secondary p-4 w-50">
-              <h4 className="card-title mx-2">Welcome back, {user.displayName}!</h4>
+              <h4 className="card-title mx-2">Why did you come back here?</h4>
               <div className="card-body p-2">
                 <p className="card-text">So, slow your roll buckaroo. You're gonna need to log out to prove that you are one  of the aforementioned dohgies/ladies</p>
               </div>
               <button type='button' className='btn btn-success w-25 align-self-center'><Link className='px-1 link-light
               text-decoration-none' to="/logout">Logout</Link></button>
-            </div> ) : (<></>)
+            </div> ) : (
+              <div className="card border-secondary p-4 w-50">
+              <h4 className="card-title mx-2">Welcome to Jilleen & Mahculls Grocery List!</h4>
+              <div className="card-body p-2">
+                <p className="card-text">So, slow your roll buckaroo. You're gonna need to log out to prove that you are one  of the aforementioned dohgies/ladies</p>
+              </div>
+              <button type='button' className='btn btn-success w-25 align-self-center'><Link className='px-1 link-light
+              text-decoration-none' to="/login">Login</Link></button>
+            </div>
+          )
         }
 
 
