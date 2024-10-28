32eb10579f57cb3f13e6bad7481360c79f4d6665
Michael McAdow <michaelmcadow@gmail.com>
2024-10-28 05:44:54 -0400

    102824

--- frontend/Groceries/src/pages/Home.jsx
+++ frontend/Groceries/src/pages/Home.jsx
@@ -1,21 +1,10 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { useAuthContext } from "../hooks/useAuthContext";
-import { useLogout } from "../hooks/useLogout";
-
-
-// const { user } = useAuthContext();
 
 export default function Home() {
 
   const { user } = useAuthContext();
-  const { logout } = useLogout();
-
-  const handleClick = () => {
-    logout();
-    // navigate("/");
-  }
-
 
   return (
     <>
@@ -29,7 +18,7 @@ export default function Home() {
                 <p className="card-text">So, slow your roll buckaroo. You're gonna need to log out to prove that you are one  of the aforementioned dohgies/ladies</p>
               </div>
               <button type='button' className='btn btn-success w-25 align-self-center'><Link className='px-1 link-light
-              text-decoration-none' to="/logout">Logout</Link></button>
+              text-decoration-none' to="/logout">Logout?</Link></button>
             </div> ) : (
               <div className="card border-secondary p-4 w-50">
               <h4 className="card-title mx-2">Welcome to Jilleen & Mahculls Grocery List!</h4>
@@ -37,25 +26,11 @@ export default function Home() {
                 <p className="card-text">So, slow your roll buckaroo. You're gonna need to log out to prove that you are one  of the aforementioned dohgies/ladies</p>
               </div>
               <button type='button' className='btn btn-success w-25 align-self-center'><Link className='px-1 link-light
-              text-decoration-none' onClick={handleClick}>Login</Link></button>
+              text-decoration-none' to="/login">Login</Link></button>
             </div>
           )
         }
-
-
-        {/* <div className="card border-secondary p-4 w-50" >
-          <h4 className="card-title mx-2">Welcome to Jilleen & Mahculls Grocery List!</h4>
-          <div className="card-body p-2">
-          <div className="card-header text-danger p-3">But only if you are either Kubrick, Maizy or Jilleen.</div>
-            <p className="card-text">So, slow your roll buckaroo. You're gonna need to login to prove that you are one
-            of the aforementioned dohgies/ladies</p>
-          </div>
-          <button type='button' className='btn btn-success w-25 align-self-center'><Link className='px-1 link-light
-          text-decoration-none' to="/login">Login</Link></button>
-        </div> */}
-
       </div>
     </>
-    
   )
 }
