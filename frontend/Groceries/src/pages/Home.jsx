a6c48a58395b1dea85fae14403e1afddff528551
Michael McAdow <michaelmcadow@gmail.com>
2024-10-29 05:45:13 -0400

    102924

--- frontend/Groceries/src/pages/Home.jsx
+++ frontend/Groceries/src/pages/Home.jsx
@@ -1,15 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { useAuthContext } from "../hooks/useAuthContext";
+import { useLogout } from "../hooks/useLogout";
 
 export default function Home() {
 
   const { user } = useAuthContext();
+  const { logout } = useLogout();
 
+  const handleClick = () => {
+    logout();
+  }
   return (
     <>
       <div className="container-fluid">
-
         {
           user ? (
             <div className="card border-secondary p-4 w-50">
@@ -18,7 +22,7 @@ export default function Home() {
                 <p className="card-text">So, slow your roll buckaroo. You're gonna need to log out to prove that you are one  of the aforementioned dohgies/ladies</p>
               </div>
               <button type='button' className='btn btn-success w-25 align-self-center'><Link className='px-1 link-light
-              text-decoration-none' to="/logout">Logout?</Link></button>
+              text-decoration-none' onClick={handleClick}>Logout?</Link></button>
             </div> ) : (
               <div className="card border-secondary p-4 w-50">
               <h4 className="card-title mx-2">Welcome to Jilleen & Mahculls Grocery List!</h4>
