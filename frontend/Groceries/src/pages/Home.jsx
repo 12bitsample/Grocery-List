c303c02e312d88141549027df70c6101c93d9d89
Michael McAdow <michaelmcadow@gmail.com>
2024-10-30 05:18:28 -0400

    103024

--- frontend/Groceries/src/pages/Home.jsx
+++ frontend/Groceries/src/pages/Home.jsx
@@ -19,18 +19,17 @@ export default function Home() {
             <div className="card border-secondary p-4 w-50">
               <h4 className="card-title mx-2">Why did you come back here?</h4>
               <div className="card-body p-2">
-                <p className="card-text">So, slow your roll buckaroo. You're gonna need to log out to prove that you are one  of the aforementioned dohgies/ladies</p>
+                <p className="card-text">No real reason to head back here, would you like to logout? </p>
               </div>
-              <button type='button' className='btn btn-success w-25 align-self-center'><Link className='px-1 link-light
-              text-decoration-none' onClick={handleClick}>Logout?</Link></button>
+              <button type="button" className="btn btn-success w-25 align-self-center"><Link className="px-1 link-light
+              text-decoration-none" onClick={handleClick}>Logout?</Link></button>
             </div> ) : (
               <div className="card border-secondary p-4 w-50">
               <h4 className="card-title mx-2">Welcome to Jilleen & Mahculls Grocery List!</h4>
               <div className="card-body p-2">
                 <p className="card-text">So, slow your roll buckaroo. You're gonna need to log out to prove that you are one  of the aforementioned dohgies/ladies</p>
               </div>
-              <button type='button' className='btn btn-success w-25 align-self-center'><Link className='px-1 link-light
-              text-decoration-none' to="/login">Login</Link></button>
+              <button type="button" className="ditto text-white btn btn-success fw-bold p-1 btn-outline-secondary my-1" ><Link to="/login">Login</Link></button>
             </div>
           )
         }
