4b0e8e2517a0a7fc179e9889777b4da998ce28aa
Michael McAdow <michaelmcadow@gmail.com>
2024-11-11 05:36:02 -0500

    111124

--- frontend/Groceries/src/hooks/useRegister.jsx
+++ frontend/Groceries/src/hooks/useRegister.jsx
@@ -16,21 +16,9 @@ import { useAuthContext } from "./useAuthContext";
                 headers: { "Content-type": "application/json" },
                 body: JSON.stringify({ email, password }),  
               });
-
-              //troubleshooting - remove later
-              console.log("Response: ", response )
       
               const json = await response.json();
               console.log("Parsed JSON", json);
-      
-              // if (!response.ok) {
-              //   //troubleshooting - remove later
-              //   console.log("Response error text: ", errorText);
-
-              //   setError(json.error || `Error: ${response.status} - ${response.statusText}`);
-              //   setIsLoading(false);
-              //   return;
-              // }
       
               dispatch({ type: "LOGIN", payload: json });
               setIsLoading(false);
