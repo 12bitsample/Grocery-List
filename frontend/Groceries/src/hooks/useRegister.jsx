cb8804ec409e4aa3bb49065ce76e8ebf67f653e9
Michael McAdow <michaelmcadow@gmail.com>
2025-02-06 14:21:37 -0500

    2625

--- frontend/Groceries/src/hooks/useRegister.jsx
+++ frontend/Groceries/src/hooks/useRegister.jsx
@@ -7,8 +7,8 @@ import { useAuthContext } from "./useAuthContext";
     const { dispatch } = useAuthContext();
 
     const register = async (email, password) => { 
-            setIsLoading(true);
-            setError(null); 
+              setIsLoading(true);
+              setError(null); 
 
               const response = await fetch("http://localhost:4000/register", {
               method: "POST",
@@ -22,7 +22,6 @@ import { useAuthContext } from "./useAuthContext";
               setError(json.error); // Set error from JSON
               setIsLoading(false);
               dispatch({ type: "LOGOUT" });
-
             } 
             if (response.ok) {
               //save user to local storage
